2a168f6396286f97afcfdbe8f5fd933a67ad46a8
mcverter <mitchell.verter@gmail.com>
2020-09-27 18:37:37 -0400

    edited sentences

---
+++ RadicalEmpiricism/src/scripts/populateSentencesDB.js
@@ -0,0 +1,10 @@
+// ti_sentences table -- index french, english, 
+
+// open ti french
+// keep a counter
+// insert line per file
+// insert (id, french) VALUES (COUNT, input )
+
+// open ti english 
+// for each line in file, populate db from both files
+// update ti_sentences, set english=input where id=COUNT
