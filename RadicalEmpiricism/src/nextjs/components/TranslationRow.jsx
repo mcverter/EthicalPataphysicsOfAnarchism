20c0723012adfc43c9909398bc32ba5f87776f94
mcverter <mitchell.verter@gmail.com>
2022-01-14 09:42:57 -0500

    LineBook column

--- RadicalEmpiricism/src/nextjs/components/TranslationRow.jsx
+++ RadicalEmpiricism/src/nextjs/components/TranslationRow.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-const EnglishCell = (english) => <td>{english}</td>;
-const FrenchCell = (french) => <td>{french}</td>;
-
 /**
- * 
- * @param param0 
- * @returns 
+ *
+ * @param {number} idx -- Line Number
+ * @param {string} book -- book name
+ * @param {string} english -- english sentence
+ * @param {string} french -- french sentence
+ * @returns {*}
+ * @constructor
  */
 export const TranslationRow = ({idx, book, english, french}) => {
   return (
   <tr id={book + idx} key={book + idx}>
-    <td width="4%" >L{idx}</td>
+    <td width="4%" >{book}#{idx}</td>
     <td width="48%" className="english"> {english} </td>
     <td width="48%" className="french"> {french} </td>
   </tr>
