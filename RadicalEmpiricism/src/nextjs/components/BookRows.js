611353a137acc8b97e3a29432dc6a06ccc5a406d
mcverter <mitchell.verter@gmail.com>
2022-01-14 10:40:40 -0500

    styles

--- RadicalEmpiricism/src/nextjs/components/BookRows.js
+++ RadicalEmpiricism/src/nextjs/components/BookRows.js
@@ -1,8 +1,15 @@
 import {TranslationRow} from "./TranslationRow";
 import React from "react";
+import Collapsible from 'react-collapsible'
+
+const LongBooknameMap = {
+    "TI": "Totality and Infinity // Totalité et infini",
+    "OTB":  "Otherwise than Being //Autrement qu'être",
+}
 
 export const BookRows  = ({bookname, sentences}) => (
-<table>
+<Collapsible trigger={LongBooknameMap[bookname]} className={bookname.toLowerCase()}>
+    <table>
     {sentences && sentences.map(s =>
         <TranslationRow
             idx={s.line}
@@ -12,4 +19,5 @@ export const BookRows  = ({bookname, sentences}) => (
         />)
     }
 </table>
+</Collapsible>
 )
