b288515cec0d84bfac02deedc1af6afa831238cb
mcverter <mitchell.verter@gmail.com>
2022-01-15 13:13:00 -0500

    fixed frequency picker

--- RadicalEmpiricism/src/nextjs/components/WordSearchForm/WordSearchForm.js
+++ RadicalEmpiricism/src/nextjs/components/WordSearchForm/WordSearchForm.js
@@ -1,69 +1,22 @@
 import Select from "react-select";
 import React from "react";
+import { WordSelecter } from "./WordSelecter";
+import { WordOrderingChooser } from "./WordOrderingChooser";
 
-export const WordSearchForm = ({selectedOption, changeSelectedWord, options, onChangeFrequencyOrder}) => (
-    <div style={{padding: '5px', border: '5px solid #eaeaea', margin: 15}}>
-        <form>
-            <label htmlFor="select-word" style={{fontSize: 24, fontWeight: 700, lineHeight: 1.5}}> Search /
-                Chercher </label>
-
-            <Select
-                defaultValue={selectedOption}
-                onChange={changeSelectedWord}
-                options={options}
-                isSearchable={true}
-                name="select-word"
-            />
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-                <div>Word Frequency Sort:</div>
-                {/*
-                <div className="radio">
-                    <label>
-                        <input
-                            type="radio"
-                            value="ti"
-                            name="frequency-type"
-                            onChange={onChangeFrequencyOrder}
-                        />
-                        Totality
-                    </label>
-                </div>
-                <div className="radio">
-                    <label>
-                        <input
-                            type="radio"
-                            value="otb"
-                            name="frequency-type"
-                            onChange={onChangeFrequencyOrder}
-                        />
-                        Otherwise
-                    </label>
-                </div>
-            */}
-                <div className="radio">
-                    <label>
-                        <input
-                            type="radio"
-                            value="both"
-                            name="frequency-type"
-                        />
-                        Both
-                    </label>
-                </div>
-                {/*
-                <div className="radio">
-                    <label>
-                        <input
-                            type="radio"
-                            value="alpha"
-                            name="frequency-type"
-                            onChange={onChangeFrequencyOrder}
-                        />
-                        A-Z
-                    </label>
-                </div>
-            */}
-            </div>
-        </form>
-    </div>
-)
+export const WordSearchForm = ({
+  selectedOption,
+  changeSelectedWord,
+  options,
+  onChangeFrequencyOrder,
+}) => (
+  <div style={{ padding: "5px", border: "5px solid #eaeaea", margin: 15 }}>
+    <form>
+      <WordSelecter
+        selectedOption={selectedOption}
+        changeSelectedWord={changeSelectedWord}
+        options={options}
+      />
+      <WordOrderingChooser onChangeFrequencyOrder={onChangeFrequencyOrder} />
+    </form>
+  </div>
+);
--- RadicalEmpiricism/src/nextjs/pages/index.js
+++ RadicalEmpiricism/src/nextjs/pages/index.js
@@ -1,85 +1,121 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import {tiFrench} from "../data/sentences/TIFrenchSentences";
-import {tiEnglish} from "../data/sentences/TIEnglishSentences";
-import {otbEnglish} from "../data/sentences/OTBEnglishSentences";
-import {otbFrench} from "../data/sentences/OTBFrenchSentences";
-import {otbFrequencyList, tiFrequencyList, combinedFrequencyList, alphabeticalList} from '../data/words/parseWordFiles'
-import {BookRows} from "../components/BookRows/BookRows";
-import {WordSearchForm} from "../components/WordSearchForm/WordSearchForm";
-import {Introduction} from "../components/Introduction";
+import { tiFrench } from "../data/sentences/TIFrenchSentences";
+import { tiEnglish } from "../data/sentences/TIEnglishSentences";
+import { otbEnglish } from "../data/sentences/OTBEnglishSentences";
+import { otbFrench } from "../data/sentences/OTBFrenchSentences";
+import {
+  otbFrequencyList,
+  tiFrequencyList,
+  combinedFrequencyList,
+  alphabeticalList,
+} from "../data/words/parseWordFiles";
+import { BookRows } from "../components/BookRows/BookRows";
+import { WordSearchForm } from "../components/WordSearchForm/WordSearchForm";
+import { Introduction } from "../components/Introduction";
 
-const createCombinedArray = ({englishSentences, frenchSentences}) => {
-    const frenchArray = frenchSentences.split('\n');
-    const englishArray = englishSentences.split('\n');
-    const combinedArray = []
-    for (let i = 0; i < frenchArray.length; i++) {
-        combinedArray.push({
-            line: i + 1,
-            english: englishArray[i],
-            french: frenchArray[i]
-        })
-    }
-    return combinedArray;
-}
+const createCombinedArray = ({ englishSentences, frenchSentences }) => {
+  const frenchArray = frenchSentences.split("\n");
+  const englishArray = englishSentences.split("\n");
+  const combinedArray = [];
+  for (let i = 0; i < frenchArray.length; i++) {
+    combinedArray.push({
+      line: i + 1,
+      english: englishArray[i],
+      french: frenchArray[i],
+    });
+  }
+  return combinedArray;
+};
 
-const tiCombinedArr = createCombinedArray({englishSentences: tiEnglish, frenchSentences: tiFrench});
+const tiCombinedArr = createCombinedArray({
+  englishSentences: tiEnglish,
+  frenchSentences: tiFrench,
+});
 
-const otbCombinedArr = createCombinedArray({englishSentences: otbEnglish, frenchSentences: otbFrench});
-
+const otbCombinedArr = createCombinedArray({
+  englishSentences: otbEnglish,
+  frenchSentences: otbFrench,
+});
 
 const defaultSentence = {
-    line: 0,
-    english: "Search sentences for matching words",
-    french: "Rechercher des phrases pour les mots correspondants"
-}
+  line: 0,
+  english: "Search sentences for matching words",
+  french: "Rechercher des phrases pour les mots correspondants",
+};
 const bookToWordFrequency = {
-    alpha: alphabeticalList,
-    ti: tiFrequencyList,
-    otb: otbFrequencyList,
-    both: combinedFrequencyList
-}
+  alpha: alphabeticalList,
+  ti: tiFrequencyList,
+  otb: otbFrequencyList,
+  both: combinedFrequencyList,
+};
 export default function Home() {
-    const [combinedTISentences, setCombinedTISentences] = useState([defaultSentence]);
-    const [combinedOTBSentences, setCombinedOTBSentences] = useState([defaultSentence]);
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [options, setOptions] = useState(combinedFrequencyList)
+  const [combinedTISentences, setCombinedTISentences] = useState([
+    defaultSentence,
+  ]);
+  const [combinedOTBSentences, setCombinedOTBSentences] = useState([
+    defaultSentence,
+  ]);
 
-    const changeWordFrequencyOrder = (ordering) => {
-        setOptions(bookToWordFrequency[ordering]);
-    }
-    const changeSelectedWord = (w) => {
-        setSelectedOption(w);
-        const {value} = w;
-        if (value.length > 2) {
-            const pattern = value;
-            const tiCombinedFiltered = tiCombinedArr.filter(s => {
-                return s.french.match(pattern)
-            })
-            const otbCombinedFiltered = otbCombinedArr.filter(s => {
-                return s.french.match(pattern)
-            })
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [options, setOptions] = useState(combinedFrequencyList);
 
-            setCombinedTISentences(tiCombinedFiltered)
-            setCombinedOTBSentences(otbCombinedFiltered)
-        }
-    }
-
-
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Levinas Radical Empiricism (Empirisme Radical) </title>
-            </Head>
-            <main>
-                <Introduction french="Levinas Empirisme Radical" english="Levinas Radical Empiricism"/>
-                <WordSearchForm options={options} changeSelectedWord={changeSelectedWord}
-                                selectedOption={selectedOption} onChangeFrequencyOrder={changeWordFrequencyOrder}/>
-                <BookRows sentences={combinedTISentences} book="TI"/>
-                <BookRows sentences={combinedOTBSentences} book="OTB"/>
-            </main>
-            <nav>Questions/Comments:  <a style={{color: "blue", textDecoration: "underline"}} target="_blank" href="mailto:roadrunner@waste.org,mitchell.verter@gmail.com?subject=RadicalEmpiricism">Me voici (Mitchell Cowen Verter email)</a></nav>
-        </div>
+  const changeWordFrequencyOrder = (event) => {
+    const ordering = bookToWordFrequency[event.target.value];
+    const foo = selectedOption;
+    const selectedOptionInNewOrdering = ordering.find(
+      (o) => o.value === selectedOption.value
     );
+    setOptions(ordering);
+    setSelectedOption(selectedOptionInNewOrdering);
+  };
+  const changeSelectedWord = (w) => {
+    setSelectedOption(w);
+    const { value } = w;
+    if (value.length > 2) {
+      const pattern = value;
+      const tiCombinedFiltered = tiCombinedArr.filter((s) => {
+        return s.french.match(pattern);
+      });
+      const otbCombinedFiltered = otbCombinedArr.filter((s) => {
+        return s.french.match(pattern);
+      });
+
+      setCombinedTISentences(tiCombinedFiltered);
+      setCombinedOTBSentences(otbCombinedFiltered);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Levinas Radical Empiricism (Empirisme Radical) </title>
+      </Head>
+      <main>
+        <Introduction
+          french="Levinas Empirisme Radical"
+          english="Levinas Radical Empiricism"
+        />
+        <WordSearchForm
+          options={options}
+          changeSelectedWord={changeSelectedWord}
+          selectedOption={selectedOption}
+          onChangeFrequencyOrder={changeWordFrequencyOrder}
+        />
+        <BookRows sentences={combinedTISentences} book="TI" />
+        <BookRows sentences={combinedOTBSentences} book="OTB" />
+      </main>
+      <nav>
+        Questions/Comments:{" "}
+        <a
+          style={{ color: "blue", textDecoration: "underline" }}
+          target="_blank"
+          href="mailto:roadrunner@waste.org,mitchell.verter@gmail.com?subject=RadicalEmpiricism"
+        >
+          Me voici (Mitchell Cowen Verter email)
+        </a>
+      </nav>
+    </div>
+  );
 }
