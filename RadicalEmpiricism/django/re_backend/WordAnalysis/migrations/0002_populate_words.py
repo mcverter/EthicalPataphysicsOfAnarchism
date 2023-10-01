d268e1ccfb66adf3e5d64297b80a5a5938f4db1b
mcverter <mitchell.verter@gmail.com>
2023-10-01 08:20:22 -0400

    migration

--- RadicalEmpiricism/django/re_backend/WordAnalysis/migrations/0002_populate_words.py
+++ RadicalEmpiricism/django/re_backend/WordAnalysis/migrations/0002_populate_words.py
@@ -1,7 +1,26 @@
+from django.db import migrations
+from ..models import Word
 WORD_MAP_PATH = '../../../../data/books/'
 WORD_MAP_FILE = '{}WordMap.txt'.format(WORD_MAP_PATH)
 
-with (open(WORD_MAP_FILE, "r", encoding="utf-8") as f):
-    lines = f.readlines()
-    for line in lines:
-        (word, ti, otb) = line.split(',')
+
+def populate_my_table(apps, schema_editor):
+    with (open(WORD_MAP_FILE, "r", encoding="utf-8") as f):
+        lines = f.readlines()
+        for line in lines:
+            word = Word()
+            (french, ti, otb) = line.split(',')
+            word.french = french
+            word.ti = ti
+            word.otb = otb
+            word.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('myapp', 'the_table_migration'),
+    ]
+
+    operations = [
+      migrations.RunPython(populate_my_table),
+    ]
