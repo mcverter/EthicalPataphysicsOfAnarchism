65dd6e3873ee7f1c7dc0b64db3710a5bbbb172a5
mcverter <mitchell.verter@gmail.com>
2023-10-06 13:30:49 -0400

    explanations

--- RadicalEmpiricism/django/scripts/english_definitions.py
+++
@@ -1,5 +0,0 @@
-from english_dictionary.scripts.read_pickle import get_dict
-english_dict = get_dict()
-
-definition = english_dict["xylophone"]  # english_dict is a Python dictionary of English
-print(definition)
