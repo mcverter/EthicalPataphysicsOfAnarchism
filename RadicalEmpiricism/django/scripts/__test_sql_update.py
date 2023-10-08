ff747c060dfecec078a61368accee678035d065d
mcverter <mitchell.verter@gmail.com>
2023-10-08 15:43:01 -0400

    refactoring

--- RadicalEmpiricism/django/scripts/__test_sql_update.py
+++ RadicalEmpiricism/django/scripts/__test_sql_update.py
@@ -2,8 +2,8 @@ import unittest
 from write_sql__update_word_tbl__english_etymology import add_english_etymology
 
 expected = 'late 14c., realtif, in grammar, "a relative pronoun," from Old French relatif (13c.), from Late Latin relativus "having reference or relation," from Latin relatus, used as past participle of referre "bring back, bear back" (see refer), from re- "back, again" + lātus "borne, carried" (see oblate (n.)). The meaning "kinsman, kinswoman, person in the same family or connected by blood" is attested from 1650s.   '
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+class SqlUpdateTests(unittest.TestCase):
+    def test_sql_update(self):
         self.assertEqual(add_english_etymology(), expected)
 
 
