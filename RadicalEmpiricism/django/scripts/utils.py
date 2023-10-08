3ea17df367bf4109c00982c3ff7cd81f9dcd9eb0
mcverter <mitchell.verter@gmail.com>
2023-10-08 13:14:16 -0400

    testing

--- RadicalEmpiricism/django/scripts/utils.py
+++ RadicalEmpiricism/django/scripts/utils.py
@@ -47,20 +47,3 @@ def add_french_definition():
 def add_french_etymology():
     pass
 
-def add_english_etymology():
-    output = ''
-    english = 'foo'
-
-    with open(ETYMONLINE_PATH, "r", encoding="utf-8") as f:
-        content = f.read()
-        content = content.replace('\n', ' ')
-        content = content.replace('\s+', ' ')
-        etymology_re = '<div class="word__etymology_expand.*?data-more'
-        etym_match = re.search(etymology_re, content)
-        raw_div = etym_match.group(0)
-        raw_div = re.sub('\s+', ' ', raw_div)
-        raw_div = re.sub('<.*?>', '', raw_div)
-        raw_div = re.sub('<a data-more', '', raw_div)
-        raw_div = re.sub('^\s+', '', raw_div)
-        output += f"UPDATE WordAnalysis_word SET english_etymology='{raw_div}' WHERE english='{english}';\n"
-        print(raw_div)
--- RadicalEmpiricism/django/scripts/write_sql__update_word_tbl__english_definition.py
+++ RadicalEmpiricism/django/scripts/write_sql__update_word_tbl__english_definition.py
@@ -9,10 +9,8 @@ def update_word_tbl__english_definition_sql():
 
     english_words = select_fields_from_word_table(["english"])
 
-    output = ''
     for word in english_words:
         definition = english_dict[word[0]]
-        output += update_word_table('english_definition', definition, 'english', word[0])
-
-    with open(sql_output_file, "w", encoding="utf-8") as g:
-        g.write(output)
+        output = update_word_table('english_definition', definition, 'english', word[0])
+        with open(sql_output_file, "w", encoding="utf-8") as g:
+            g.write(output)
