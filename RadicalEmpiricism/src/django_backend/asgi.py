bea307a770724a38454f32cfe5ab2d8ffeeaa8be
mcverter <mitchell.verter@gmail.com>
2023-11-11 15:07:12 -0500

    renaming django backend

--- RadicalEmpiricism/src/django_backend/asgi.py
+++ RadicalEmpiricism/src/django_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for re_backend project.
+ASGI config for django_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 're_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
 
 application = get_asgi_application()
