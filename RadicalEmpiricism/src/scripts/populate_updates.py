0e9721913fd907a4c71caefb88cc6ccce81d3796
mcverter <mitchell.verter@gmail.com>
2023-12-08 00:22:00 +0700

    multiprocessing fk updates

--- RadicalEmpiricism/src/scripts/populate_updates.py
+++ RadicalEmpiricism/src/scripts/populate_updates.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process
+
 from .populate_translations import populate_translations
 from .populate_definitions import populate_definitions
 from .populate_etymologies import populate_etymologies
@@ -5,8 +7,12 @@ from .populate_etymologies import populate_etymologies
 
 def populate_updates():
     populate_translations()
-    populate_definitions()
-    populate_etymologies()
+    p1 = Process(target=populate_definitions)
+    p2 = Process(target=populate_etymologies)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
 
 
 if __name__ == '__main__':
