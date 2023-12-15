a2a3b42aca0e6da3d26aa1c4414b0594fe7d0079
mcverter <mitchell.verter@gmail.com>
2023-12-15 23:29:13 +0700

    maybe rest framework

---
+++ RadicalEmpiricism/src/word_analysis/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
+
+"""
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+# Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+"""
