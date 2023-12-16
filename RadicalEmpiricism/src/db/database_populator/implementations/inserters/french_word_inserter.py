bb3b621a4a444071a6dc48a6102df8568ea5fe18
mcverter <mitchell.verter@gmail.com>
2023-12-16 21:23:18 +0700

    populate scripts process

--- RadicalEmpiricism/src/db/database_populator/implementations/inserters/french_word_inserter.py
+++ RadicalEmpiricism/src/db/database_populator/implementations/inserters/french_word_inserter.py
@@ -6,7 +6,7 @@ from RadicalEmpiricism.src.constants import TABLE_WORD, COLUMN_FRENCH, OTB_FRENC
     COLUMN_TI, COLUMN_OTB, TABLE_BOOK_LINE
 from RadicalEmpiricism.src.db.database_populator.database_inserter import DatabaseInserter
 from RadicalEmpiricism.src.db.db import insert_into_table, insert_many_to_many, \
-    select_composite_id, commit_all, select_single_value
+    select_composite_id, select_single_value
 from RadicalEmpiricism.src.utils import is_empty_value
 
 PUNCTUATION_MARKS = '[/.,()!?"]'
@@ -81,7 +81,7 @@ class FrenchWordInserter(DatabaseInserter):
                 lines = json.load(file)
                 for idx in range(len(lines)):
                     insert_into_table(TABLE_BOOK_LINE, ("book", "line"), (book, idx))
-        commit_all()
+        self.commit(666666)
 
     def populate(self):
         self.create_lines_table()
@@ -101,6 +101,7 @@ class FrenchWordInserter(DatabaseInserter):
             values = (key,
                       self.WORD_MAP[key][COLUMN_TI]['count'],
                       self.WORD_MAP[key][COLUMN_OTB]['count'])
+            insert_into_table(TABLE_WORD, ("french", "ti", "otb"), values)
 
             if inner_counter % 50 == 6:
                 self.commit(counter)
@@ -124,30 +125,6 @@ class FrenchWordInserter(DatabaseInserter):
         self.commit(counter)
 
 
-'''
-
-
-    def populate(self):
-        with open(CLEANED_TOTALITE, "r", encoding="utf-8") as ti:
-            ti_content = ti.read()
-            ti_words = split_into_words(ti_content)
-            add_words_to_map(ti_words, "ti")
-
-        with open(CLEANED_AUTREMENT, "r", encoding="utf-8") as otb:
-            otb_content = otb.read()
-            otb_words = split_into_words(otb_content)
-            add_words_to_map(otb_words, "otb")
-
-        counter = 0
-        for row in [(key[0], key[1]["ti"], key[1]["otb"]) for key in WORD_MAP.items()]:
-            if row[0] != "parabreak":
-                self.insert_single_item(row)
-                counter += 1
-                if counter % 50 == 14:
-                    self.commit(counter)
-
-        self.commit(counter)
-'''
 
 if __name__ == '__main__':
     updater = FrenchWordInserter()
--- RadicalEmpiricism/src/scripts/populate_updates.py
+++ RadicalEmpiricism/src/scripts/populate_updates.py
@@ -1,17 +1,38 @@
 from multiprocessing import Process
 
-from RadicalEmpiricism.src.scripts.populate_translations import populate_translations
-from RadicalEmpiricism.src.scripts.populate_definitions import populate_definitions
-from RadicalEmpiricism.src.scripts.populate_etymologies import populate_etymologies
+from RadicalEmpiricism.src.db.database_populator.implementations.foreign_key_updaters.definition.english_definition_updater import \
+    EnglishDefinitionUpdater
+from RadicalEmpiricism.src.db.database_populator.implementations.foreign_key_updaters.definition.french_definition_updater import \
+    FrenchDefinitionUpdater
+from RadicalEmpiricism.src.db.database_populator.implementations.foreign_key_updaters.etymology.english_etymology_updater import \
+    EnglishEtymologyUpdater
+from RadicalEmpiricism.src.db.database_populator.implementations.foreign_key_updaters.etymology.french_etymology_updater import \
+    FrenchEtymologyUpdater
+from RadicalEmpiricism.src.db.database_populator.implementations.native_table_updaters.translation.english_translation_updater import \
+    EnglishTranslationUpdater
 
-DO_TRANSLATION = True
+def populate_english_updates():
+    EnglishTranslationUpdater().populate()
+    p1 = Process(target=EnglishDefinitionUpdater().populate)
+    p2 = Process(target=EnglishEtymologyUpdater().populate)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+
+def populate_french_updates():
+    p1 = Process(target=FrenchDefinitionUpdater().populate)
+    p2 = Process(target=FrenchEtymologyUpdater().populate)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
 
 
 def populate_updates():
-    if DO_TRANSLATION:
-        populate_translations()
-    p1 = Process(target=populate_definitions)
-    p2 = Process(target=populate_etymologies)
+    p1 = Process(target=populate_french_updates)
+    p2 = Process(target=populate_english_updates)
     p1.start()
     p2.start()
     p1.join()
