836431933f7c1b57e1aa17f7bd109068ef58c28e
mcverter <mitchell.verter@gmail.com>
2023-12-28 12:27:24 -0500

    template tags

--- word_analysis/templatetags/radical_empiricism_templates.py
+++ word_analysis/templatetags/radical_empiricism_templates.py
@@ -2,9 +2,12 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('pages/word_list_page.html')
-def word_list_header_title(value: str, arg: str):
-    (french, english) = arg.split(",")
-    return '''
-    <div> hello </div>
-    '''
+@register.inclusion_tag('tags/nav_item.html')
+def nav_item(value: str):
+    (french, english) = value.split(",")
+    return {"french": french, "english": english}
+
+@register.inclusion_tag('tags/word_list_header_item.html')
+def word_list_header_item(value: str):
+    (french, english) = value.split(",")
+    return {"french": french, "english": english}
