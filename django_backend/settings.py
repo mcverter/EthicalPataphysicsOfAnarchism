441461c9652de5b1541af08265d18a86064fbd8e
mcverter <mitchell.verter@gmail.com>
2023-12-28 21:43:56 -0500

    bootstrap

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "word_analysis",
     'corsheaders',
+    'django_sass'
 ]
 
 MIDDLEWARE = [
--- word_analysis/templatetags/radical_empiricism_templates.py
+++ word_analysis/templatetags/radical_empiricism_templates.py
@@ -2,12 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('tags/nav_item.html')
-def nav_item(value: str):
-    (french, english) = value.split(",")
-    return {"french": french, "english": english}
-
-@register.inclusion_tag('tags/word_list_header_item.html')
-def word_list_header_item(value: str):
+@register.inclusion_tag('tags/french_english.html')
+def french_english(value: str):
     (french, english) = value.split(",")
     return {"french": french, "english": english}
