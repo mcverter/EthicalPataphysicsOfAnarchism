26cbe75e65637a8773010d769a066b47858a1309
mcverter <mitchell.verter@gmail.com>
2023-12-31 18:50:24 -0500

    group membership page

--- word_analysis/urls.py
+++ word_analysis/urls.py
@@ -6,6 +6,7 @@ from .views import word_detail, words, groups, technical, summary, relations
 urlpatterns = [
                   re_path(r"^$", summary, name="summary"),
                   re_path(r"^relations$", relations, name="relations"),
+                  re_path(r"^membership$", membership, name="membership"),
                   re_path(r"^summary|resume$", summary, name="summary"),
                   re_path(r"^groups|groupes$", groups, name="groups"),
                   re_path(r"^technical|technique$", technical, name="technical"),
