66b1c6ade73673796376f7d7dc75043a5957d8cb
mcverter <mitchell.verter@gmail.com>
2024-01-02 07:44:23 -0500

    cleaning up template tags

--- word_analysis/templatetags/radical_empiricism_templates.py
+++ word_analysis/templatetags/radical_empiricism_templates.py
@@ -7,12 +7,8 @@ register = template.Library()
 def fe_card(value: str):
     return {"component_name": value}
 
-@register.inclusion_tag('tags/french_english.html')
-def french_english(value: str):
-    (french, english) = value.split("@")
-    return {"french": french, "english": english}
 
-@register.inclusion_tag('tags/french_english_span.html')
-def french_english_span(value: str):
+@register.inclusion_tag('tags/french_english.html')
+def fe_vertical(value: str):
     (french, english) = value.split("@")
     return {"french": french, "english": english}
--- word_analysis/templatetags/radical_empricism_filters.py
+++ word_analysis/templatetags/radical_empricism_filters.py
@@ -1,8 +1,7 @@
 from django import template
-from django.template.defaultfilters import stringfilter
-from django.utils.html import conditional_escape, mark_safe
 
 register = template.Library()
+
 
 @register.filter()
 def filter_by_letter(value_dict: dict, letter: str):
@@ -10,12 +9,8 @@ def filter_by_letter(value_dict: dict, letter: str):
         return value_dict
     return [v for v in value_dict if v.startswith(letter)]
 
+
 @register.inclusion_tag('tags/french_english.html')
 def french_english(value: str):
-    (french, english) = value.split("@")
-    return {"french": french, "english": english}
-
-@register.inclusion_tag('tags/french_english_span.html')
-def french_english_span(value: str):
     (french, english) = value.split("@")
     return {"french": french, "english": english}
