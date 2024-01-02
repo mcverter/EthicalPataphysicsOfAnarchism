1d49ced00342846d1b454de8413c08220b44346e
mcverter <mitchell.verter@gmail.com>
2024-01-02 17:19:41 -0500

    second heroku

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -30,7 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
                  'levinas-radical-empiricism-aa0fc6fe9dbc.herokuapp.com',
-                 'levinasradicalempiricism.com']
+                 'levinasradicalempiricism.com',
+                 'levinas-empirisme-radical-56386a1e25f9.herokuapp.com'
+                 'levinasempirismeradical.com']
 
 # Application definition
 
