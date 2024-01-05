0223e3c59500cbb2ad2cc3ee88838e5b57d83cc5
mcverter <mitchell.verter@gmail.com>
2024-01-05 07:12:23 -0500

    translation

--- word_analysis/templatetags/radical_empricism_filters.py
+++ word_analysis/templatetags/radical_empricism_filters.py
@@ -4,6 +4,16 @@ register = template.Library()
 
 
 @register.filter()
+def starts_with(text: str, prefix: str):
+    return text.startswith(prefix)
+
+
+@register.filter()
+def does_not_start_with(text: str, prefix: str):
+    return not text.startswith(prefix)
+
+
+@register.filter()
 def filter_by_letter(value_dict: dict, letter: str):
     if not letter:
         return value_dict
