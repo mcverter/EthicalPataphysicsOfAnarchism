b003379102f4f632f5bbb59609535012a6a766c9
mcverter <mitchell.verter@gmail.com>
2024-01-08 12:43:12 -0500

    hardcoding sentences

---
+++ word_analysis/hardcoded/sentences/get_sentence.py
@@ -0,0 +1,33 @@
+from otb_en import OTB_EN
+from otb_fr import OTB_FR
+from ti_en import TI_EN
+from ti_fr import TI_FR
+
+book_language_map = {
+    "otb": {
+        "fr": OTB_FR,
+        "en": OTB_EN,
+    },
+    "ti": {
+        "fr": TI_FR,
+        "en": TI_EN,
+    }
+}
+
+def get_line(book, line):
+    return {
+        "en": book_language_map[book]["en"][line],
+        "fr": book_language_map[book]["fr"][line],
+    }
+
+def get_line_span(book, start, end):
+    return {
+        "en": book_language_map[book]["en"][start:end],
+        "fr": book_language_map[book]["fr"][start:end],
+    }
+
+def get_lines_all(book):
+    return {
+        "en": book_language_map[book]["en"],
+        "fr": book_language_map[book]["fr"],
+    }
