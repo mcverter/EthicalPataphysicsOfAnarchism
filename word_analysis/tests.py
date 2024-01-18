1d3d14b8707b36108ebc2b70150736c40bb18193
mcverter <mitchell.verter@gmail.com>
2024-01-18 13:40:48 -0500

    testing word routes.  using mot instead of word

--- word_analysis/tests.py
+++ word_analysis/tests.py
@@ -1,3 +1,5 @@
+import os
+import re
 from django.test import TestCase, Client
 from .hardcoded.site_nav_data import nav_items
 
@@ -6,7 +8,9 @@ c = Client()
 
 class RouteTests(TestCase):
     def route_test(self, route):
-        response = c.get("/" + route)
+        if not route.startswith("/"):
+            route = "/" + route
+        response = c.get(route)
         print('route', route, 'response_code', response.status_code)
         assert response.status_code == 200 or response.status_code == 301, f"route {route} failed"
 
@@ -16,11 +20,24 @@ class RouteTests(TestCase):
         print(all_subroutes)
         return sum(all_subroutes, all_toproutes)
 
-    def test_nav_bar_routes(self):
+    def z_test_nav_bar_routes(self):
         all_routes = self.gather_all_routes(nav_items)
         print(all_routes)
         for route in all_routes:
             self.route_test(route)
+
+    def check_word_routes_in_file(self, filename):
+        with open(filename, 'r', encoding='utf-8') as f:
+            content = f.read()
+            all_matches = re.findall('/mot/\w+', content)
+            for m in all_matches:
+                print("testing route", m)
+                self.route_test(m)
+
+    def test_word_routes_in_objective_file(self):
+        objective_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                      'templates/content/objective.html')
+        self.check_word_routes_in_file(objective_file)
 
 
 routes = [
