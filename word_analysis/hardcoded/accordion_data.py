bb9fa92ff67816c7082c181e1ee059b64a568bb2
mcverter <mitchell.verter@gmail.com>
2024-01-20 15:17:35 -0500

    rethinking nav

---
+++ word_analysis/hardcoded/accordion_data.py
@@ -0,0 +1,14 @@
+# map of route to title
+accordion_data = {
+    "route": "",
+    "genre_set_intro": "",
+    "duality": "",
+    "combination": "",
+    "repetition": "",
+    "opposition": "",
+    "intersection": "",
+    "parallelism": "",
+    "flat": "",
+    "two_dimensional": "",
+    "Constellation": "",
+}
