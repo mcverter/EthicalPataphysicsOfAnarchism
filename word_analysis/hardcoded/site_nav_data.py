c6fceaba2d38ef90d4662232b7ea83c8775d5aa1
mcverter <mitchell.verter@gmail.com>
2024-01-22 14:52:10 -0500

    reorganizing summary page

--- word_analysis/hardcoded/site_nav_data.py
+++ word_analysis/hardcoded/site_nav_data.py
@@ -19,10 +19,16 @@ nav_items = [
         "A discussion of the Radical Empiricism Project, in which the advantages of a multidimensional tensor analysis of metaphorical manifolds is dimensionaland the errors of unidimensional assertions of rank are critiqued"),
      "subitems": [
          {"route": "objective", "title": _("Objective"), "explanation": _("Objective")},
+         {"route": "ambisexuality", "title": _("Ambisexuality"), "explanation": _("Ambisexuality")},
+         {"route": "home_and_position", "title": _("home_and_position"), "explanation": _("home_and_position")},
+         {"route": "womb", "title": _("womb"), "explanation": _("womb")},
+         {"route": "hostage", "title": _("hostage"), "explanation": _("hostage")},
+
          {"route": "two_dimensional", "title": _("Two-Dimensional Polar Relations and Two-by-Two Matrices"),
           "explanation": _("Two-Dimensional Polar Relations and Two-by-Two Matrices")},
          {"route": "flat", "title": _("The flat space of totality"), "explanation": _("The flat space of totality")},
          {"route": "three", "title": _("Three-Dimensional Analysis"), "explanation": _("Three-Dimensional Analysis")},
+         {"route": "discussion", "title": _("Discussion"), "explanation": _("Discussion")},
      ]},
     {"route": "genre_theory", "title": _("Theory of Genre Sets"), "explanation": _(
         "This page features many discussion: (1) An Introduction to Genre Sets, (2) An example analysis of a sentence from Totality and Infinity as a constellation of Genre Sets, (3) Genre Set Membership Rules [suffixes, prefixes, etymological roots, infinities, other morphemic roots, semantic themes], (4)Internal and External Relations [duality, combination, repetition, opposition, intersection], (5) Non-Euclidean Relations [parallelism, flat, two_dimensional, constellation, curvature ], (5) Other mathematical and scientific topics [addition, positive, negative, 0, 1, 2, 3, 4, infinity, set theory, continuity and infinitesimals, topology, Mikowski/Einstein space-time, wave and quantum theory  ] (6) Perspectivism [perversion, inversion, subversion, reversion] (7) Reading Totality and Infinity as Genesis and Otherwise than Being as Exodus"),
