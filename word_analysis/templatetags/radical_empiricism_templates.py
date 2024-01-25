16ef4f6da0bc9141613ac275480c55a0fcfc651f
mcverter <mitchell.verter@gmail.com>
2024-01-25 18:39:21 -0500

    multilevel menu

--- word_analysis/templatetags/radical_empiricism_templates.py
+++ word_analysis/templatetags/radical_empiricism_templates.py
@@ -3,82 +3,71 @@ from django import template
 from constants import ABBREV_TO_FULL_TITLE_MAP
 from word_analysis.hardcoded.all_genres_data import all_genres_to_words
 from word_analysis.hardcoded.sentences.get_sentence import get_sentence
-from word_analysis.hardcoded.constellation_example_data import constellation_example_data
-from word_analysis.hardcoded.site_nav_data import nav_items, get_page_title_for_route
+from word_analysis.hardcoded.constellation_example_data import (
+    constellation_example_data,
+)
+from word_analysis.hardcoded.site_nav_data import (
+    nav_items,
+    get_page_title_for_route,
+)
 from word_analysis.hardcoded.accordion_data import accordion_route_to_title_data
 
 register = template.Library()
 
 
-@register.inclusion_tag('tags/content_accordion.html')
+@register.inclusion_tag("tags/content_accordion.html")
 def content_accordion(route):
     return {"route": route, "title": accordion_route_to_title_data[route]}
 
 
-@register.inclusion_tag('nav/page_toc.html')
+@register.inclusion_tag("nav/page_toc.html")
 def page_toc(page_title, toc_sections):
     return {"page_title": page_title, "toc_sections": toc_sections}
 
 
-@register.inclusion_tag('nav/site_map_accordion.html')
+@register.inclusion_tag("nav/site_map_accordion.html")
 def site_map_accordion():
     return {"nav_items": nav_items}
 
 
-@register.inclusion_tag('nav/nav_bar.html')
+@register.inclusion_tag("nav/nav_bar.html")
 def nav_bar():
     return {"nav_items": nav_items}
 
 
-@register.inclusion_tag('nav/nav_bar_item.html')
-def nav_bar_item(item, classes=""):
-    return {"item": item, "classes": classes}
-
-@register.inclusion_tag('nav/nav_bar_subitem.html')
-def nav_bar_subitem(item, classes=""):
-    return {"item": item, "classes": classes}
-
-
-@register.inclusion_tag('nav/nav_bar_item_with_subitems.html')
-def nav_bar_item_with_subitems(item):
-    return {"item": item}
-
-
-@register.inclusion_tag('tags/book_line_display.html')
+@register.inclusion_tag("tags/book_line_display.html")
 def book_line_display(book, line):
     return {
         "book": ABBREV_TO_FULL_TITLE_MAP[book],
-        "lines": [{
-            "num": line,
-            "sentence": get_sentence(book, line)
-        }]}
+        "lines": [{"num": line, "sentence": get_sentence(book, line)}],
+    }
 
 
-@register.inclusion_tag('tags/constellation_example.html')
+@register.inclusion_tag("tags/constellation_example.html")
 def constellation_example():
     return {"constellation_example": constellation_example_data}
 
 
-@register.inclusion_tag('tags/word_categorization_card.html')
+@register.inclusion_tag("tags/word_categorization_card.html")
 def word_categorization_card(wc: str):
     return {"wc": wc}
 
 
-@register.inclusion_tag('tags/word_btn.html')
+@register.inclusion_tag("tags/word_btn.html")
 def word_btn(value: str):
     return {"word": value}
 
 
-@register.inclusion_tag('tags/genre_btn_collapse.html')
+@register.inclusion_tag("tags/genre_btn_collapse.html")
 def genre_btn_collapse(genre: str):
     return {"genre": genre, "words": all_genres_to_words(genre)}
 
 
-@register.inclusion_tag('tags/bilingual_card.html')
+@register.inclusion_tag("tags/bilingual_card.html")
 def bilingual_card(value: str):
     return {"component": value}
 
 
-@register.inclusion_tag('tags/content_card.html')
+@register.inclusion_tag("tags/content_card.html")
 def content_card(route: str):
     return {"route": route, "title": get_page_title_for_route(route)}
--- word_analysis/views.py
+++ word_analysis/views.py
@@ -5,30 +5,35 @@ from constants import OTB, TI
 from .hardcoded.all_genres_data import all_genres_to_words, all_words_to_genres
 from .hardcoded.all_references_books import ALL_REFERENCE_BOOKS
 from .hardcoded.all_words_get import all_words_with_counts, proportion_ti_to_otb
-from .hardcoded.temp_site_nav_data import get_subitems_for_route, get_page_title_for_route
+from .hardcoded.site_nav_data import get_subitems_for_route, get_page_title_for_route
 from .models import Word
 
 
 # top level pages
 def genre_theory(request):
-    return render(request,
-                  'pages/genre_theory.html',
-                  {"toc_sections": get_subitems_for_route("genre_theory"),
-                   "page_title": get_page_title_for_route("genre_theory")
-                   })
+    return render(
+        request,
+        "pages/genre_theory.html",
+        {
+            "toc_sections": get_subitems_for_route("genre_theory"),
+            "page_title": get_page_title_for_route("genre_theory"),
+        },
+    )
 
 
 def summary(request):
-    return render(request,
-                  'pages/summary_page.html',
-                  {
-                      "toc_sections": get_subitems_for_route("summary"),
-                      "page_title": get_page_title_for_route("summary")
-                  })
+    return render(
+        request,
+        "pages/summary_page.html",
+        {
+            "toc_sections": get_subitems_for_route("summary"),
+            "page_title": get_page_title_for_route("summary"),
+        },
+    )
 
 
 def technical(request):
-    return render(request, 'pages/technical_page.html', {"books": ALL_REFERENCE_BOOKS})
+    return render(request, "pages/technical_page.html", {"books": ALL_REFERENCE_BOOKS})
 
 
 # list and detail views
@@ -36,21 +41,23 @@ def word_list(request):
     context = {
         "words": all_words_with_counts(),
     }
-    return render(request, 'pages/word_list_page.html', context)
+    return render(request, "pages/word_list_page.html", context)
 
 
 def word_detail(request, word):
-    book_word = Word.objects.select_related('etymology', 'definition').get(french=word)
+    book_word = Word.objects.select_related("etymology", "definition").get(french=word)
     if book_word is None:
-        book_word = Word.objects.select_related('etymology', 'definition').get(english=word)
+        book_word = Word.objects.select_related("etymology", "definition").get(
+            english=word
+        )
     if book_word is None:
         return "could not find that word"
     etymology = book_word.etymology
     definition = book_word.definition
 
     all_lines = book_word.book_line.all()
-    otb_lines = all_lines.order_by('line').filter(book=OTB)
-    ti_lines = all_lines.order_by('line').filter(book=TI)
+    otb_lines = all_lines.order_by("line").filter(book=OTB)
+    ti_lines = all_lines.order_by("line").filter(book=TI)
     genres = all_words_to_genres(book_word)
 
     context = {
@@ -63,37 +70,42 @@ def word_detail(request, word):
         },
         "genres": genres,
         "etymology": etymology,
-        "definition": definition
+        "definition": definition,
     }
-    return render(request, 'pages/word_detail_page.html', context)
+    return render(request, "pages/word_detail_page.html", context)
 
 
 def genre_detail(request, genre):
-    return render(request, 'pages/genre_list_page.html', {
-        "genres": all_genres_to_words,
-        "genre": genre
-    })
+    return render(
+        request,
+        "pages/genre_list_page.html",
+        {"genres": all_genres_to_words, "genre": genre},
+    )
 
 
 def genre_list(request):
-    return render(request, 'pages/genre_list_page.html', {
-        "genres": all_genres_to_words(),
-    })
+    return render(
+        request,
+        "pages/genre_list_page.html",
+        {
+            "genres": all_genres_to_words(),
+        },
+    )
 
 
 def word_list_by_prefix(request, prefix):
     context = {
         "words": all_words_with_counts(),
     }
-    return render(request, 'pages/word_list_page.html', context)
+    return render(request, "pages/word_list_page.html", context)
 
 
 # to display templates/content as a separate page
 def content_page(request, name):
-    print('content', name)
-    return render(request, 'pages/content_page.html', {'content': name})
+    print("content", name)
+    return render(request, "pages/content_page.html", {"content": name})
 
 
 # for debugging UI
 def debug_test(request):
-    return render(request, 'pages/debug_test.html')
+    return render(request, "pages/debug_test.html")
