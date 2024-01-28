39003fa2e7f80b8ea26fdad8f787c46a0fcb13cd
mcverter <mitchell.verter@gmail.com>
2024-01-28 18:49:05 -0500

    formatting

--- word_analysis/templatetags/radical_empricism_filters.py
+++ word_analysis/templatetags/radical_empricism_filters.py
@@ -10,16 +10,6 @@ def normalize_french_word(word: str):
 
 
 @register.filter()
-def starts_with(text: str, prefix: str):
-    return text.startswith(prefix)
-
-
-@register.filter()
-def does_not_start_with(text: str, prefix: str):
-    return text is not None and not text.startswith(prefix)
-
-
-@register.filter()
 def filter_by_letter(value_dict: dict, letter: str):
     if not letter:
         return value_dict
